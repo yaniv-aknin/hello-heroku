c1facedf9326f06d34c6a2d16e72a10d0b4af08b
Yaniv Aknin <yaniv@aknin.name>
2012-01-02 19:00:20 +0200

    remove the need for Heroku database settings injection

--- djproj/settings.py
+++ djproj/settings.py
@@ -1,5 +1,12 @@
 import os
+import sys
+import urlparse
 import socket
+
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
+urlparse.uses_netloc.append('sqlite')
+
 env = lambda key, returntype=str: returntype(os.environ[key])
 
 DEBUG = env('DJANGO_DEBUG', bool)
@@ -24,16 +31,24 @@ INSTALLED_APPS = (
     'supstream',
 )
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+# HEROKU_NO_DJANGO_SETTINGS
+DATABASES = dict(default={})
+def parse_database_url(database, environment_variable='DATABASE_URL'):
+    url = urlparse.urlparse(os.environ['DATABASE_URL'])
+    database.update({
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+        'ENGINE' : {
+            'postgres': 'django.db.backends.postgresql_psycopg2',
+            'mysql': 'django.db.backends.mysql',
+            'sqlite': 'django.db.backends.sqlite3',
+        }[url.scheme],
+    })
+parse_database_url(DATABASES['default'])
+del parse_database_url
 
 DOMAIN = env('DOMAIN')
 if os.environ['NETLOC_SUFFIX']:
